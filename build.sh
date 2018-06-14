e57965f19df5b7487073b9606ec2879aa15fc6e5
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-06-14 10:15:31 +0000

    add github token

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 mkdocs build --clean
-git clone --branch gh-pages https://github.com/tomochain/docs.git gh-pages
+git clone --branch gh-pages https://${GH_TOKEN}@github.com/tomochain/docs.git gh-pages
 rsync -av site/ gh-pages/
 cd gh-pages
 git config user.name "Travis CI"
