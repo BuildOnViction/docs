ef9b6fd0e5e4da5fcace0a36d1a2b97532af3e21
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2018-06-14 10:04:01 +0000

    add deployment for the docs

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+mkdocs build --clean
+git clone --branch gh-pages https://github.com/tomochain/docs.git gh-pages
+rsync -av site/ gh-pages/
+cd gh-pages
+git config user.name "Travis CI"
+git config user.email "thanhson1085@gmail.com"
+git add .
+git commit -m "Deploy from Travis CI"
+git push
